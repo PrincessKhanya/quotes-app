e825e3e22366c180f7bddeaa1f844f07902542d0
Nokukhanya Makhanya <nokhanyamakhanya@gmail.com>
2022-06-09 22:07:12 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+import './style.css'
+import Alpine from 'alpinejs'
+import LoveCounter from './love-counter';
+import Quotes from './quotes'
+import './quote.css'
+
+window.Alpine = Alpine
+Alpine.data('loveCounter', LoveCounter);
+
+Alpine.data('quoteApp', Quotes)
+
+Alpine.start()
+
+document.querySelector('#app').innerHTML = `
+  I 💚 Alpine JS!
+`
+
